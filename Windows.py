10262eb62fd83d85e8b4b806aa3799544a2fa04c
Kamil Krzyśków <34622465+kamilkrzyskow@users.noreply.github.com>
2024-03-05 17:01:41 +0100

    Update Windows.py

--- Windows.py
+++ Windows.py
@@ -1,23 +1,33 @@
 import os
 
-library_names = ("cairo-2.dll", "cairo.dll", "libcairo-2.dll")
+print(f"\n---Running {os.path.basename(__file__)}---\n")
+
+library_names = ("cairo-2", "cairo", "libcairo-2")
 filenames = ("libcairo.so.2", "libcairo.2.dylib", "libcairo-2.dll")
-found_path = ""
+first_found = ""
+names = []
 
 for name in library_names:
-    for path in os.environ['PATH'].split(os.pathsep):
-        resolved_path = os.path.join(path, name)
-        print(resolved_path)
-        if os.path.exists(resolved_path):
-            filenames = (resolved_path, ) + filenames
-            found_path = resolved_path
-            break
-    if found_path:
-        break
-else:
-    found_path = "not found"
+    if name.lower().endswith(".dll"):
+        names += [name]
+    else:
+        names += [name, name + ".dll"]
 
-print(f"The path is {found_path}")
+for name in names:
+    for path in os.environ["PATH"].split(os.pathsep):
+        resolved_path = os.path.join(path, name)
+        if os.path.exists(resolved_path):
+            print(f"Found: {resolved_path}")
+            if not first_found:
+                first_found = resolved_path
+            continue
+        print(f"Doesn't exist: {resolved_path}")
+    print("---")
+
+if first_found:
+    filenames = (first_found,) + filenames
+
+print(f"The path is {first_found or 'not found'}")
 print("List of files that FFI will try to load:")
 for filename in filenames:
     print("-", filename)
